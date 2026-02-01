2211cbc4bf8e5964f09b327478a58e4718cdeb2d
ALBERT <albertgbeaca@icloud.com>
2026-02-01 15:42:59 +0000

    .

--- server/server.js
+++ server/server.js
@@ -1,7 +1,15 @@
 import express from 'express';
 import cors from 'cors';
-import { createClient } from '@supabase/supabase-js';
 import 'dotenv/config';
+import { createClient } from '@supabase/supabase-js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
 
 const app = express();
 app.use(cors());
@@ -31,3 +39,4 @@ app.post('/submit-guestbook', async (req, res) => {
 });
 
 app.listen(3000, () => console.log('Server running on port 3000'));
+
